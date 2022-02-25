5897ecd4289fcb9de4a6ef4f88580bbad63995d2
PaulRomanov <ramosha@mail.ru>
2022-02-25 13:33:55 +0300

    add first component 2-3 tasks

--- src/app/components/arithmetic-operations/arithmetic-operations.component.ts
+++ src/app/components/arithmetic-operations/arithmetic-operations.component.ts
@@ -1,5 +1,5 @@
-import { Component, EventEmitter, Output } from '@angular/core';
-import { interval, map, take } from 'rxjs';
+import { Component } from '@angular/core';
+import { filter, interval, map, take } from 'rxjs';
 
 @Component({
   selector: 'fw-arithmetic-operations',
@@ -11,17 +11,34 @@ export class ArithmeticOperationsComponent {
 
   public numbers = interval(1000).pipe(take(20));
 
-  public valueEl$: any
-  public valueEl: Array<number> = []
+  public valueEl$: any;
+  public valueEl: Array<number> = [];
+  public valueElCut: Array<number> = [];
+  public valueElDivided: Array<number> = [];
 
   constructor() { }
 
   public valueMultipliedByTree() {
    this.valueEl$ =  this.numbers.pipe(map((element: number) => element * 3))
     .subscribe((element: number) => {
-      this.valueEl.push(element)
+      this.valueEl = [...this.valueEl, element] // this.valueEl.push(element)
+    });
+  }
+
+  public firstSevenValues() {
+    this.valueEl$ =  this.numbers.pipe(take(7))
+    .subscribe((element: number) => {
+      this.valueElCut = [...this.valueElCut, element]
+    });
+  }
+
+  public dividedByTwo(){
+    this.valueEl$ =  this.numbers.pipe(filter(element => element % 2 == 0))
+    .subscribe((element: number) => {
+      this.valueElDivided = [...this.valueElDivided, element]
     });
   }
 
 }
+
 
