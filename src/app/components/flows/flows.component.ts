ce09fae96a67c432e4352cbf7f153297e419cb2b
PaulRomanov <ramosha@mail.ru>
2022-02-28 15:09:01 +0300

    add second component 2 task

--- src/app/components/flows/flows.component.ts
+++ src/app/components/flows/flows.component.ts
@@ -8,14 +8,11 @@ import { concat, concatMap, delay, interval, map, of, repeat, Subscription, swit
 })
 export class FlowsComponent {
   public numbers = interval(1000).pipe(take(20));
-  public numbers2 = interval(100).pipe(take(20));
 
   public valueFlows$!: Subscription;
+
   public valueFlowsDelayOf200: Array<number> = [];
   public valueNewFlowElements: Array<number> = [];
-  public valueTest: Array<number> = [];
-
-  public newValueFlows$!: Subscription;
 
   constructor() { }
 
@@ -29,46 +26,15 @@ export class FlowsComponent {
       });
   }
 
-
-
   public newFlowElements() {
-
-    this.newValueFlows$ = this.numbers2
+    this.valueFlows$ = this.numbers
+      .pipe(
+        concatMap(() => interval(100).pipe(take(10)))
+      )
       .subscribe((element: number) => {
         this.valueNewFlowElements = [...this.valueNewFlowElements, element]
-
-        // concat(this.valueFlowsDelayOf200, this.valueNewFlowElements)
-        // this.valueFlowsDelayOf200.concat(this.valueNewFlowElements)
-
-        // this.valueTest = ([] as any[]).concat(this.newValueFlows$, this.valueFlows$)
       });
-
-
   }
-
-  public test() {
-    // this.delayOf200()
-    // this.newFlowElements()
-
-    // this.valueTest = this.valueTest.concat(this.valueNewFlowElements , this.valueFlowsDelayOf200)
-    this.valueTest = ([] as any[]).concat(this.valueNewFlowElements , this.valueNewFlowElements)
-
-
-    // concat(this.newValueFlows$, this.valueFlows$).subscribe(value => {
-    //   console.log(value);
-    // });
-
-    // concat(this.newValueFlows$, this.newValueFlows$)
-    // .pipe(toArray())
-    // .subscribe(value => {
-    //   console.log('is array = ', Array.isArray(value));
-    //   console.log(`value = `, value);
-    // });
-
-
-  }
-
-
 
   ngOnDestroy() {
     this.valueFlows$.unsubscribe();
