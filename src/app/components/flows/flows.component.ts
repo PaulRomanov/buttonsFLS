7c82180902845033224bb879f2f2600ec1363bb8
PaulRomanov <ramosha@mail.ru>
2022-02-28 12:55:32 +0300

    add second component 2 task

--- src/app/components/flows/flows.component.ts
+++ src/app/components/flows/flows.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { concatMap, delay, interval, map, of, repeat, Subscription, switchMap, take } from 'rxjs';
+import { concat, concatMap, delay, interval, map, of, repeat, Subscription, switchMap, take, toArray } from 'rxjs';
 
 @Component({
   selector: 'fw-flows',
@@ -8,24 +8,69 @@ import { concatMap, delay, interval, map, of, repeat, Subscription, switchMap, t
 })
 export class FlowsComponent {
   public numbers = interval(1000).pipe(take(20));
+  public numbers2 = interval(100).pipe(take(20));
 
   public valueFlows$!: Subscription;
   public valueFlowsDelayOf200: Array<number> = [];
+  public valueNewFlowElements: Array<number> = [];
+  public valueTest: Array<number> = [];
+
+  public newValueFlows$!: Subscription;
 
   constructor() { }
 
   public delayOf200() {
     this.valueFlows$ = this.numbers
-    .pipe(
-      switchMap(item => of(item).pipe(delay(200), repeat(20)))
-    )
-    .subscribe((element: number) => {
-         this.valueFlowsDelayOf200 = [...this.valueFlowsDelayOf200, element]
-       });
-   }
+      .pipe(
+        switchMap(item => of(item).pipe(delay(200), repeat(20)))
+      )
+      .subscribe((element: number) => {
+        this.valueFlowsDelayOf200 = [...this.valueFlowsDelayOf200, element]
+      });
+  }
 
 
-   ngOnDestroy() {
+
+  public newFlowElements() {
+
+    this.newValueFlows$ = this.numbers2
+      .subscribe((element: number) => {
+        this.valueNewFlowElements = [...this.valueNewFlowElements, element]
+
+        // concat(this.valueFlowsDelayOf200, this.valueNewFlowElements)
+        // this.valueFlowsDelayOf200.concat(this.valueNewFlowElements)
+
+        // this.valueTest = ([] as any[]).concat(this.newValueFlows$, this.valueFlows$)
+      });
+
+
+  }
+
+  public test() {
+    // this.delayOf200()
+    // this.newFlowElements()
+
+    // this.valueTest = this.valueTest.concat(this.valueNewFlowElements , this.valueFlowsDelayOf200)
+    this.valueTest = ([] as any[]).concat(this.valueNewFlowElements , this.valueNewFlowElements)
+
+
+    // concat(this.newValueFlows$, this.valueFlows$).subscribe(value => {
+    //   console.log(value);
+    // });
+
+    // concat(this.newValueFlows$, this.newValueFlows$)
+    // .pipe(toArray())
+    // .subscribe(value => {
+    //   console.log('is array = ', Array.isArray(value));
+    //   console.log(`value = `, value);
+    // });
+
+
+  }
+
+
+
+  ngOnDestroy() {
     this.valueFlows$.unsubscribe();
   }
 
